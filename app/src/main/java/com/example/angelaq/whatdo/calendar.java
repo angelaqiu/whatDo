8b6ce723c7131aaee5d12fc10e179e62d013bb41
madelinewu <madelinewu0720@gmail.com>
2015-09-12 02:45:06 -0400

    ok back to calendar + mapsactivity changes

--- app/src/main/java/com/example/angelaq/whatdo/calendar.java
+++ app/src/main/java/com/example/angelaq/whatdo/calendar.java
@@ -1,7 +1,91 @@
 package com.example.angelaq.whatdo;
 
+import android.os.AsyncTask;
+import android.provider.CalendarContract;
+import android.util.EventLog;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.util.DateTime;
+
+import com.google.api.services.calendar.model.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Created by Madeline on 9/12/2015.
  */
-public class calendar {
+public class calendar extends AsyncTask<Void, Void, Void>{
+    private MapsActivity mActivity;
+    /**
+     * Constructor.
+     * @param activity MainActivity that spawned this task.
+     */
+    void ApiAsyncTask(MapsActivity activity) {
+        this.mActivity = activity;
+    }
+
+    /**
+     * Background task to call Google Calendar API.
+     * @param params no parameters needed for this task.
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            mActivity.clearResultsText();
+            mActivity.updateResultsText(getDataFromApi());
+
+        } catch (final GooglePlayServicesAvailabilityIOException availabilityException) {
+            mActivity.showGooglePlayServicesAvailabilityErrorDialog(
+                    availabilityException.getConnectionStatusCode());
+
+        } catch (UserRecoverableAuthIOException userRecoverableException) {
+            mActivity.startActivityForResult(
+                    userRecoverableException.getIntent(),
+                    MapsActivity.REQUEST_AUTHORIZATION);
+
+        } catch (Exception e) {
+            mActivity.updateStatus("The following error occurred:\n" +
+                    e.getMessage());
+        }
+        if (mActivity.mProgress.isShowing()) {
+            mActivity.mProgress.dismiss();
+        }
+        return null;
+    }
+
+    /**
+     * Fetch a list of the next 10 events from the primary calendar.
+     * @return List of Strings describing returned events.
+     * @throws IOException
+     */
+    private List<String> getDataFromApi() throws IOException {
+        // List the next 10 events from the primary calendar.
+        DateTime now = new DateTime(System.currentTimeMillis());
+        List<String> eventStrings = new ArrayList<String>();
+        CalendarContract.Events events = mActivity.mService.events().list("primary")
+                .setMaxResults(10)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+        List<EventLog.Event> items = events.getItems();
+
+        for (EventLog.Event event : items) {
+            DateTime start = event.getStart().getDateTime();
+            if (start == null) {
+                // All-day events don't have start times, so just use
+                // the start date.
+                start = event.getStart().getDate();
+            }
+            eventStrings.add(
+                    String.format("%s (%s)", event.getSummary(), start));
+        }
+        return eventStrings;
+    }
+
 }
+
